9d8cd51d8ca868cd21aeac1dacf574c46e1d7d97
Samu <samu.lyhty@gmail.com>
2025-09-30 20:18:32 +0300

    rermoved unnecessary logging

--- src/controllers/GroupController.js
+++ src/controllers/GroupController.js
@@ -263,15 +263,11 @@ export const addMemberToGroup = async (req, res) => {
   try {
     const { gID } = req.params;
     
-    // Debug logging
-    console.log('Request body:', req.body);
-    console.log('Content-Type:', req.get('Content-Type'));
-    
     // Check if req.body exists
     if (!req.body || Object.keys(req.body).length === 0) {
       return res.status(400).json({
         success: false,
-        error: 'Request body is required. Make sure Content-Type is application/json'
+        error: 'Request body is required. Make sure Content-Type is application/json and body format is raw JSON'
       });
     }
     
